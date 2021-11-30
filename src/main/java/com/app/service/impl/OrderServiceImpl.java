eec22372c702c445287a3f9556114b8c665596af
ranjeet.suman <ranjeet.suman@ext.deposit-solutions.com>
2021-11-30 21:53:45 +0530

    harcode rest api url removed

--- src/main/java/com/app/service/impl/OrderServiceImpl.java
+++ src/main/java/com/app/service/impl/OrderServiceImpl.java
@@ -7,15 +7,22 @@ import com.app.entity.Order;
 import com.app.repo.OrderRepository;
 import com.app.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
+@RefreshScope
 public class OrderServiceImpl implements OrderService {
     @Autowired
     private OrderRepository orderRepository;
     @Autowired
+    @Lazy
     private RestTemplate restTemplate;
+    @Value("${microservice.payment-service.endpoints.endpoint.uri}")
+    private String ENDPOINT_URL;
     @Override
     public TransactionResponse saveOrder(TransactionRequest transactionRequest) {
         String response="";
@@ -24,7 +31,7 @@ public class OrderServiceImpl implements OrderService {
         payment.setOrderId(order.getId());
         payment.setPrice(order.getPrice());
         //rest call
-        Payment paymentResponse = restTemplate.postForObject("http://localhost:9596/payment/doPayment", payment, Payment.class);
+        Payment paymentResponse = restTemplate.postForObject(ENDPOINT_URL, payment, Payment.class);
         response = paymentResponse.getPaymentStatus().equals("success") ?"payment processing and order is placed":"there is failure in payment api, items are added into the cart";
         orderRepository.save(order);
         return new TransactionResponse(order, paymentResponse.getPrice(), paymentResponse.getTransactionId(),response);
